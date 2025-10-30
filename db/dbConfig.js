a875404f56a5a48b859730bcc1b60dbf4d5e21f8
Tg-kebeta <tigist.chefessa@gmail.com>
2025-10-29 22:51:23 -0400

    Refactor database connection configuration

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,14 +1,15 @@
-const mysql = require("mysql2/promise");
+const mysql2 = require("mysql2");
+require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+const dbConnection = mysql2.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: 3306, // or whatever port they give you
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.DB_PORT,
+  connectionLimit: 11,
 });
 
-module.exports = pool;
+
+
+module.exports = dbConnection.promise();
